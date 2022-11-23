40e40b9bc0bc4450a442044de38b95c4089b775d
gameboo <alexandre.joannou@gmail.com>
2022-11-23 18:47:28 +0100

    restructured a bit

--- src/Blarney/AXI4.hs
+++ src/Blarney/AXI4.hs
@@ -1,9 +1,9 @@
 module Blarney.AXI4 (
   module Blarney.AXI4TypesCommon
 , module Blarney.AXI4.Types
-, module Blarney.AXI4.BasicUtils
+, module Blarney.AXI4.Utils
 ) where
 
 import Blarney.AXI4TypesCommon
 import Blarney.AXI4.Types
-import Blarney.AXI4.BasicUtils
+import Blarney.AXI4.Utils
--- src/Blarney/AXI4/Utils/Basics.hs
+++ src/Blarney/AXI4/Utils/Basics.hs
@@ -1,36 +1,35 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PolyKinds #-}
 
-module Blarney.AXI4.BasicUtils (
+module Blarney.AXI4.Utils.Basics (
+  mkAXI4BufferShimFF
 ) where
 
 import Blarney
+import Blarney.Queue
+import Blarney.SourceSink
 import Blarney.AXI4.Types
 
 -- | AXI4 buffer shim
 
 mkAXI4BufferShim_Core ::
-  -- AXI4 parameters
-  forall (params :: AXI4_Params id_bits addr_bits data_bytes
-                                awuser_bits wuser_bits buser_bits
-                                aruser_bits ruser_bits).
   -- type aliases and constraints
   ( awflit ~ AXI4_AWFlit id_bits addr_bits awuser_bits
   , wflit ~ AXI4_WFlit data_bytes wuser_bits
   , bflit ~ AXI4_BFlit id_bits buser_bits
   , arflit ~ AXI4_ARFlit id_bits addr_bits aruser_bits
   , rflit ~ AXI4_RFlit id_bits data_bytes ruser_bits
+  , params ~ (_dummy :: AXI4_Params id_bits addr_bits data_bytes
+                                    awuser_bits wuser_bits buser_bits
+                                    aruser_bits ruser_bits)
   , ToSource aw_buff awflit, ToSink aw_buff awflit
   , ToSource w_buff wflit, ToSink w_buff wflit
   , ToSource b_buff bflit, ToSink b_buff bflit
   , ToSource ar_buff arflit, ToSink ar_buff arflit
   , ToSource r_buff rflit, ToSink r_buff rflit )
   -- argument types and return type
-  => Module (aw_buff awflit)
-  -> Module (w_buff wflit)
-  -> Module (b_buff bflit)
-  -> Module (ar_buff arflit)
-  -> Module (r_buff rflit)
+  => Module aw_buff -> Module w_buff -> Module b_buff
+  -> Module ar_buff -> Module r_buff
   -> Module (AXI4_Shim params params)
 mkAXI4BufferShim_Core mkAWBuff mkWBuff mkBBuff mkARBuff mkRBuff = do
   awbuff <- mkAWBuff
@@ -50,4 +49,8 @@ mkAXI4BufferShim_Core mkAWBuff mkWBuff mkBBuff mkARBuff mkRBuff = do
                                                     ,  r = toSource rbuff } }
 
 mkAXI4BufferShimFF :: Module (AXI4_Shim params params)
-mkAXI4BufferShimFF = mkAXI4BufferShim_Core mkFIFO mkFIFO mkFIFO mkFIFO mkFIFO
+mkAXI4BufferShimFF = mkAXI4BufferShim_Core makeQueue
+                                           makeQueue
+                                           makeQueue
+                                           makeQueue
+                                           makeQueue
